29d42513114f728a3b222e05e28342265f810037
Abdullah221 <abdullahwaris59@gmail.com>
2025-04-13 00:05:23 +0500

    Miss Neelam Info JavaScript File Strucutre Creation

--- FreeCodeCamp/01_HTML/Day 5 Full HTML Practice/companyInfo.js
+++ FreeCodeCamp/01_HTML/Day 5 Full HTML Practice/companyInfo.js
@@ -1,5 +1,58 @@
-let companyName = document.getElementById("company-name-full");
-let companyShortName = document.getElementsByClassName("company-name");
-
-companyName.textContent = "Miss Neelam Cosmetics";
-companyShortName.textContent = "Miss Neelam";
+let companyName = "Miss Neelam";
+let companyFullName = "Miss Neelam Cosmetics";
+let Products = [{
+    productName: "Miss Neelam Gold",
+    productDescription: "For Face Washing",
+    productFormula: "Glycern Etc.",
+    productPrice: "PKR: 250",
+    productImgs: {
+        productImgOne: "one",
+        productImgTwo: "two",
+        productImgThree: "three",
+    },
+    productVideo: "src",
+},{
+    productName: "Miss Neelam Gold",
+    productDescription: "For Face Washing",
+    productFormula: "Glycern Etc.",
+    productPrice: "PKR: 250",
+    productImgs: {
+        productImgOne: "one",
+        productImgTwo: "two",
+        productImgThree: "three",
+    },
+    productVideo: "src",
+},{
+    productName: "Miss Neelam Gold",
+    productDescription: "For Face Washing",
+    productFormula: "Glycern Etc.",
+    productPrice: "PKR: 250",
+    productImgs: {
+        productImgOne: "one",
+        productImgTwo: "two",
+        productImgThree: "three",
+    },
+    productVideo: "src",
+},{
+    productName: "Miss Neelam Gold",
+    productDescription: "For Face Washing",
+    productFormula: "Glycern Etc.",
+    productPrice: "PKR: 250",
+    productImgs: {
+        productImgOne: "one",
+        productImgTwo: "two",
+        productImgThree: "three",
+    },
+    productVideo: "src",
+},{
+}
+];
+let aboutUs = {
+    companyName: companyName,
+    companyIntro: "Description Will be here",
+};
+let contact = {
+    contactNumber : "+92",
+    contactEmail : "@gmail.com",
+    contactAddress: "25000, KPK PAK."
+}
