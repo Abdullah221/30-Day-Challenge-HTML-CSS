7433ce10c943239007ef87eb0dff65b632892111
Abdullah221 <abdullahwaris59@gmail.com>
2025-04-12 23:52:07 +0500

    Miss Neelam file initialize structure

---
+++ FreeCodeCamp/01_HTML/Day 5 Full HTML Practice/companyInfo.js
@@ -0,0 +1,5 @@
+let companyName = document.getElementById("company-name-full");
+let companyShortName = document.getElementsByClassName("company-name");
+
+companyName.textContent = "Miss Neelam Cosmetics";
+companyShortName.textContent = "Miss Neelam";
